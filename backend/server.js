96f5f7e24b618ca87ee3cf47cb6788e763710cbf
MrAtul2k5 <atulbusinessanalyst7@gmail.com>
2026-03-23 16:11:15 +0530

    Backend updated

--- backend/server.js
+++ backend/server.js
@@ -171,7 +171,7 @@ app.post("/api/polls/:id/vote", async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
